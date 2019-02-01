fa8643e30ead7da0c3ac819d6aaa81060cfc9dbe
Jakob Pennington <jakobpennington@gmail.com>
2019-02-01 12:07:04 +1030

    Moved SSL scanning to SCAN

--- Scripts/scan.py
+++ Scripts/scan.py
@@ -7,14 +7,12 @@
 #  increasingly thorough and targeted scanning.                                 #
 #                                                                               #
 #  Author: Jakob Pennington                                                     #
-#  Version: 1.0.0                                                               #
-#  Last modified: 16-05-2018                                                    #
-#                                                                               #
-#  TODO: Everything                                                             #
+#  Version: 1.0.1                                                               #
+#  Last modified: 01-02-2019                                                    #
 #                                                                               #
 #################################################################################
 name = 'SCAN'
-version = '1.0.0'
+version = '1.0.1'
 tagline = 'Perform initial scanning against a target.'
 
 #####################
@@ -24,6 +22,7 @@ import argparse
 import os
 import subprocess
 import sys
+
 from Modules import helpers
 
 #####################
@@ -45,6 +44,7 @@ def main():
         sys.exit('Exiting.')
 
     # Orchestrate scanning components
+    scan_ssl()
     nmap_fast()
     scan_web()
     nmap_all_ports()
@@ -62,6 +62,162 @@ def nmap_fast():
     helpers.print_task_positive('Fast nmap scanning')
     output_nmap_fast = subprocess.check_output(['nmap', '-sV', '-O', args.ip, '-oA', 'nmap/nmap-fast'], universal_newlines=True)
     print()
+
+def scan_ssl():
+        helpers.print_heading('Beginning SSL Scanning...')
+        # Create  directories for output files
+        subprocess.run(['mkdir', 'ssl'])
+        subprocess.run(['mkdir', 'ssl/scan-output'])
+        subprocess.run(['mkdir', 'ssl/vulnerabilities'])
+
+        # Read the file
+        with open(os.getcwd() + '/' + args.list, newline=None) as file:
+            for line in file:
+                # Strip newlines
+                hostname = line.rstrip('\n')
+                print(hostname)
+
+                # Capture the sslscan output
+                output_sslscan = subprocess.check_output(['sslscan', hostname], universal_newlines=True)
+
+                # Remove ANSI encoding so we can search
+                output_sslscan_clean = helpers.remove_ansi(output_sslscan)
+
+                # Process the SSLScan output
+                # SSL / TLS versions
+                # SSLv2
+                if (output_sslscan_clean.find('SSLv2') != -1):
+                    helpers.print_task_positive('SSLv2')
+                    vuln_file = open('ssl/vulnerabilities/tls-sslv2-0.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # SSLv3
+                if (output_sslscan_clean.find('SSLv3') != -1):
+                    helpers.print_task_positive('SSLv3')
+                    vuln_file = open('ssl/vulnerabilities/tls-sslv3-0.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # TLSV1.0
+                if (output_sslscan_clean.find('TLSv1.0') != -1):
+                    helpers.print_task_positive('TLSv1.0')
+                    vuln_file = open('ssl/vulnerabilities/tls-tlsv1-0.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # TLS Fallback SCSV
+                if (output_sslscan_clean.find('Server does not support TLS Fallback SCSV') != -1):
+                    helpers.print_task_positive('TLS Fallback SCSV')
+                    vuln_file = open('ssl/vulnerabilities/tls-fallback-scsv.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # TLS CRIME
+                if (output_sslscan_clean.find('Compression enabled (CRIME)') != -1):
+                    helpers.print_task_positive('Compression enabled (CRIME)')
+                    vuln_file = open('ssl/vulnerabilities/tls-compression-crime.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Certificate issues
+                # Expired certificate
+                if (helpers.remove_control_characters(output_sslscan).find('Not valid after:  [31m') != -1):
+                    helpers.print_task_positive('Expired certificate')
+                    vuln_file = open('ssl/vulnerabilities/certificate-expired.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Wildcard certificate
+                if (output_sslscan_clean.find('Subject:  *') != -1):
+                    helpers.print_task_positive('Wildcard certificate')
+                    vuln_file = open('ssl/vulnerabilities/certificate-wildcard.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Weak certificate signature
+                if (output_sslscan_clean.find('sha1WithRSA') != -1):
+                    helpers.print_task_positive('Weak certificate signature')
+                    vuln_file = open('ssl/vulnerabilities/certificate-weak-signature.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Cipher suites
+                # Export ciphers
+                if (output_sslscan_clean.find('EXP-') != -1):
+                    helpers.print_task_positive('Weak cipher - Export cipher')
+                    vuln_file = open('ssl/vulnerabilities/weak-cipher-export.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # RC4
+                if (output_sslscan_clean.find('RC4') != -1):
+                    helpers.print_task_positive('Weak cipher - RC4')
+                    vuln_file = open('ssl/vulnerabilities/weak-cipher-rc4.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # DES-CBC(3)-SHA
+                if (output_sslscan_clean.find('DES-CBC') != -1):
+                    helpers.print_task_positive('Weak cipher - DES-CBC')
+                    vuln_file = open('ssl/vulnerabilities/weak-cipher-des-cbc.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Weak diffie hellman
+                if (output_sslscan_clean.find('DHE 1024 bits') != -1):
+                    helpers.print_task_positive('Weak cipher - DHE 1024 bits')
+                    vuln_file = open('ssl/vulnerabilities/weak-cipher-dhe-1024.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+                    
+
+                # Capture the sslyze output
+                output_sslyze = subprocess.check_output(['sslyze', '--regular', '--hsts', hostname], universal_newlines=True)
+
+                # Process the sslyze output (Only what is missed by SSLScan)
+                # Client initiated renegotiation
+                if (output_sslyze.find('VULNERABLE - Server honors client-initiated renegotiations') != -1):
+                    helpers.print_task_positive('Server honors client-initiated renegotiations')
+                    vuln_file = open('ssl/vulnerabilities/tls-client-initiated-renegotiations.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Missing HSTS header
+                if (output_sslyze.find('NOT SUPPORTED - Server did not send an HSTS header.') != -1):
+                    helpers.print_task_positive('HSTS Unsupported')
+                    vuln_file = open('ssl/vulnerabilities/tls-hsts-unsupported.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Self-signed certificate
+                if (output_sslyze.find('FAILED - Certificate is NOT Trusted: self signed certificate') != -1):
+                    helpers.print_task_positive('FAILED - Certificate is NOT Trusted: self signed certificate')
+                    vuln_file = open('ssl/vulnerabilities/tls-self-signed-certificate.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Mismatched certificate
+                if (output_sslyze.find('FAILED - Certificate does NOT') != -1):
+                    helpers.print_task_positive('FAILED - Certificate does NOT match')
+                    vuln_file = open('ssl/vulnerabilities/tls-mismatched-certificate.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Untrusted certificate
+                if (output_sslyze.find('FAILED - Certificate is NOT Trusted') != -1):
+                    helpers.print_task_positive('FAILED - Certificate is NOT Trusted')
+                    vuln_file = open('ssl/vulnerabilities/tls-untrusted-certificate.txt', 'a')
+                    vuln_file.write(str(hostname) + '\n')
+                    vuln_file.close()
+
+                # Write the output to file
+                hostname = hostname.replace('.','-')
+                outfile = open('ssl/scan-output/ssl-' + hostname + '.txt', 'w')
+                outfile.write(str(output_sslscan))
+                outfile.write(str(output_sslyze))
+                outfile.close()
+
 
 def scan_web():
     # Prepare for web scanning
--- Scripts/whoare.py
+++ Scripts/whoare.py
@@ -11,6 +11,9 @@
 #  Last modified: 01-02-2019                                                    #
 #                                                                               #
 #################################################################################
+name = 'WHOARE'
+version = '1.0.1'
+tagline = 'Gathering passive information for a list of hostnames.'
 
 #####################
 # IMPORTS
@@ -29,21 +32,22 @@ from Modules import helpers
 # FUNCTIONS
 #####################
 def main():
+    # Print messages
+    helpers.print_script_message(name, version, tagline)
+    helpers.print_task_positive('Hold tight...')
+
     # Check that targets have been supplied
     if (not args.host and not args.list):
         sys.exit('You must supply a target or list of targets. See help (-h) for more details.')
 
     # Disable all scanning if flags have been supplied
-    if (args.ssl or args.theharvester or args.whois or args.dns):
+    if (args.theharvester or args.whois or args.dns):
         args.all = False
 
     # Process hostnames into domains / subdomains etc
     process_hostnames()
 
     # Run scans based on supplied flags
-    if (args.all or args.ssl):
-        scan_ssl()
-
     if (args.all or args.whois):
         scan_whois()
 
@@ -140,163 +144,6 @@ def scan_dns():
     hostname_ip_outfile.close()
 
 
-# TODO Move to Poke
-def scan_ssl():
-        helpers.print_heading('Beginning SSL Scanning...')
-        # Create  directories for output files
-        subprocess.run(['mkdir', 'ssl'])
-        subprocess.run(['mkdir', 'ssl/scan-output'])
-        subprocess.run(['mkdir', 'ssl/vulnerabilities'])
-
-        # Read the file
-        with open(os.getcwd() + '/' + args.list, newline=None) as file:
-            for line in file:
-                # Strip newlines
-                hostname = line.rstrip('\n')
-                print(hostname)
-
-                # Capture the sslscan output
-                output_sslscan = subprocess.check_output(['sslscan', hostname], universal_newlines=True)
-
-                # Remove ANSI encoding so we can search
-                output_sslscan_clean = helpers.remove_ansi(output_sslscan)
-
-                # Process the SSLScan output
-                # SSL / TLS versions
-                # SSLv2
-                if (output_sslscan_clean.find('SSLv2') != -1):
-                    helpers.print_task_positive('SSLv2')
-                    vuln_file = open('ssl/vulnerabilities/tls-sslv2-0.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # SSLv3
-                if (output_sslscan_clean.find('SSLv3') != -1):
-                    helpers.print_task_positive('SSLv3')
-                    vuln_file = open('ssl/vulnerabilities/tls-sslv3-0.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # TLSV1.0
-                if (output_sslscan_clean.find('TLSv1.0') != -1):
-                    helpers.print_task_positive('TLSv1.0')
-                    vuln_file = open('ssl/vulnerabilities/tls-tlsv1-0.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # TLS Fallback SCSV
-                if (output_sslscan_clean.find('Server does not support TLS Fallback SCSV') != -1):
-                    helpers.print_task_positive('TLS Fallback SCSV')
-                    vuln_file = open('ssl/vulnerabilities/tls-fallback-scsv.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # TLS CRIME
-                if (output_sslscan_clean.find('Compression enabled (CRIME)') != -1):
-                    helpers.print_task_positive('Compression enabled (CRIME)')
-                    vuln_file = open('ssl/vulnerabilities/tls-compression-crime.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Certificate issues
-                # Expired certificate
-                if (helpers.remove_control_characters(output_sslscan).find('Not valid after:  [31m') != -1):
-                    helpers.print_task_positive('Expired certificate')
-                    vuln_file = open('ssl/vulnerabilities/certificate-expired.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Wildcard certificate
-                if (output_sslscan_clean.find('Subject:  *') != -1):
-                    helpers.print_task_positive('Wildcard certificate')
-                    vuln_file = open('ssl/vulnerabilities/certificate-wildcard.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Weak certificate signature
-                if (output_sslscan_clean.find('sha1WithRSA') != -1):
-                    helpers.print_task_positive('Weak certificate signature')
-                    vuln_file = open('ssl/vulnerabilities/certificate-weak-signature.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Cipher suites
-                # Export ciphers
-                if (output_sslscan_clean.find('EXP-') != -1):
-                    helpers.print_task_positive('Weak cipher - Export cipher')
-                    vuln_file = open('ssl/vulnerabilities/weak-cipher-export.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # RC4
-                if (output_sslscan_clean.find('RC4') != -1):
-                    helpers.print_task_positive('Weak cipher - RC4')
-                    vuln_file = open('ssl/vulnerabilities/weak-cipher-rc4.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # DES-CBC(3)-SHA
-                if (output_sslscan_clean.find('DES-CBC') != -1):
-                    helpers.print_task_positive('Weak cipher - DES-CBC')
-                    vuln_file = open('ssl/vulnerabilities/weak-cipher-des-cbc.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Weak diffie hellman
-                if (output_sslscan_clean.find('DHE 1024 bits') != -1):
-                    helpers.print_task_positive('Weak cipher - DHE 1024 bits')
-                    vuln_file = open('ssl/vulnerabilities/weak-cipher-dhe-1024.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-                    
-
-                # Capture the sslyze output
-                output_sslyze = subprocess.check_output(['sslyze', '--regular', '--hsts', hostname], universal_newlines=True)
-
-                # Process the sslyze output (Only what is missed by SSLScan)
-                # Client initiated renegotiation
-                if (output_sslyze.find('VULNERABLE - Server honors client-initiated renegotiations') != -1):
-                    helpers.print_task_positive('Server honors client-initiated renegotiations')
-                    vuln_file = open('ssl/vulnerabilities/tls-client-initiated-renegotiations.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Missing HSTS header
-                if (output_sslyze.find('NOT SUPPORTED - Server did not send an HSTS header.') != -1):
-                    helpers.print_task_positive('HSTS Unsupported')
-                    vuln_file = open('ssl/vulnerabilities/tls-hsts-unsupported.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Self-signed certificate
-                if (output_sslyze.find('FAILED - Certificate is NOT Trusted: self signed certificate') != -1):
-                    helpers.print_task_positive('FAILED - Certificate is NOT Trusted: self signed certificate')
-                    vuln_file = open('ssl/vulnerabilities/tls-self-signed-certificate.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Mismatched certificate
-                if (output_sslyze.find('FAILED - Certificate does NOT') != -1):
-                    helpers.print_task_positive('FAILED - Certificate does NOT match')
-                    vuln_file = open('ssl/vulnerabilities/tls-mismatched-certificate.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Untrusted certificate
-                if (output_sslyze.find('FAILED - Certificate is NOT Trusted') != -1):
-                    helpers.print_task_positive('FAILED - Certificate is NOT Trusted')
-                    vuln_file = open('ssl/vulnerabilities/tls-untrusted-certificate.txt', 'a')
-                    vuln_file.write(str(hostname) + '\n')
-                    vuln_file.close()
-
-                # Write the output to file
-                hostname = hostname.replace('.','-')
-                outfile = open('ssl/scan-output/ssl-' + hostname + '.txt', 'w')
-                outfile.write(str(output_sslscan))
-                outfile.write(str(output_sslyze))
-                outfile.close()
-
-
 def scan_the_harvester():
         helpers.print_heading('Beginning theharvester Scanning...')
 
@@ -353,7 +200,6 @@ target_group.add_argument('-L', '--list', help='A file containing a list of host
 scan_group = parser.add_argument_group("Scan Types")
 scan_group.add_argument('-a', '--all', action='store_true', default=True, help='Run all scans (Default. Supply scan flags to enable specific scans.).')
 scan_group.add_argument('-d', '--dns', action='store_true', help='Enable DNS scanning.')
-scan_group.add_argument('-s', '--ssl', action='store_true', help='Enable SSL scanning.')
 scan_group.add_argument('-t', '--theharvester', action='store_true', help='Enable theharvester scanning.')
 scan_group.add_argument('-w', '--whois', action='store_true', help='Enable WhoIS scanning.')
 # Parse the supplied arguments
